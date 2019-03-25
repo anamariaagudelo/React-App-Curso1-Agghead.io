fcbe2987a26fe888b706d5750a0c4d5f8f903ac4
anamariaagudelo <anamariaagudeloisi@gmail.com>
2019-03-24 20:02:09 -0500

    Video6- Componentes como hijos de otros componentes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
+  }
+
+  update(e) {
+    this.setState({ txt: e.target.value })
+
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)}/>
+        
       </div>
-    );
+
+    )
   }
 }
+
+const Widget = (props) =>
+<input type="text" onChange={props.update} />
+
 
 export default App;
